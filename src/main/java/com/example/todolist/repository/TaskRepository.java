0a754addf177c19030559b07457d34b50a07378c
Tom047 <tangiev.murad@mail.ru>
2023-12-20 03:46:46 +0600

    findAllByUser() method add

--- src/main/java/com/example/todolist/repository/TaskRepository.java
+++ src/main/java/com/example/todolist/repository/TaskRepository.java
@@ -1,9 +1,13 @@
 package com.example.todolist.repository;
 
 import com.example.todolist.entity.Task;
+import com.example.todolist.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findAllByUser(User user);
 }
