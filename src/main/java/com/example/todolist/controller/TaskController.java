707d67bf2e4cc283b4490983241372dc341baf5d
Tom047 <tangiev.murad@mail.ru>
2023-12-19 16:41:28 +0600

    RequestMapping address change

--- src/main/java/com/example/todolist/controller/TaskController.java
+++ src/main/java/com/example/todolist/controller/TaskController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tasks")
+@RequestMapping("/api/v1/tasks")
 @AllArgsConstructor
 public class TaskController {
 
