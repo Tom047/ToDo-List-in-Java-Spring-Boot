5496e0721e1b5138b819d3a4a4b392b178a74bf1
Tom047 <tangiev.murad@mail.ru>
2023-12-20 02:05:15 +0600

    AuthController add

---
+++ src/main/java/com/example/todolist/security/AuthController.java
@@ -0,0 +1,61 @@
+package com.example.todolist.security;
+
+import com.example.todolist.dto.UserDTO;
+import com.example.todolist.security.jwt.JwtRequest;
+import com.example.todolist.security.jwt.JwtResponse;
+import com.example.todolist.security.jwt.JwtService;
+import com.example.todolist.service.UserDetailsServiceImpl;
+import com.example.todolist.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtService jwtService;
+    private final AuthenticationManager authManager;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> createAuthToken(@RequestBody UserDTO userDTO) {
+        try {
+            userService.create(userDTO);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userDTO.getUsername());
+
+        String token = jwtService.generateToken(userDetails);
+
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> authenticate(@RequestBody JwtRequest authRequest) {
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authRequest.getUsername(),
+                        authRequest.getPassword()
+                )
+        );
+
+//        User user = userService.readByUsername(authRequest.getUsername());
+//        UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
+//
+//        String jwt = jwtService.generateToken(userDetails);
+
+        return ResponseEntity.ok(authRequest);
+    }
+}
